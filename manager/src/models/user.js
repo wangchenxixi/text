d02081af8710a8781c163202b8129d7709d06d2a
unknown <923772138@qq.com>
2019-06-11 20:26:27 +0800

    验证

---
+++ manager/src/models/user.js
@@ -0,0 +1,23 @@
+import { login } from "../services";
+export default {
+    namespace: 'user',
+    state: {},
+    subscriptions: {
+        setup({ dispatch, history }) { },
+    },
+    effects: {
+        *login({ payload }, { call, put }) {
+            console.log('payload..', payload, login);
+            let data = yield call(login);
+            console.log("data...", data)
+        },
+        *fetch({ payload }, { call, put }) {
+            yield put({ type: 'save' })
+        },
+    },
+    reducers: {
+        save(state, action) {
+            return { ...state, ...action.payload }
+        }
+    }
+}
--- manager/src/utils/request.js
+++ manager/src/utils/request.js
@@ -1,30 +1,21 @@
-import fetch from 'dva/fetch';
-
-function parseJSON(response) {
-  return response.json();
-}
-
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
+import axios from "axios";
+import { promises } from "fs";
+const service=axios.create({
+  baseURL:'http://169.254.12.77:7001/',
+  timeout:5000
+})
+service.interceptors.request.use(
+  config=>{
+    return config
+  },
+  error=>{
+    return Promise.reject(error)
   }
-
-  const error = new Error(response.statusText);
-  error.response = response;
-  throw error;
-}
-
-/**
- * Requests a URL, returning a promise.
- *
- * @param  {string} url       The URL we want to request
- * @param  {object} [options] The options we want to pass to "fetch"
- * @return {object}           An object containing either "data" or "err"
- */
-export default function request(url, options) {
-  return fetch(url, options)
-    .then(checkStatus)
-    .then(parseJSON)
-    .then(data => ({ data }))
-    .catch(err => ({ err }));
-}
+)
+service.interceptors.response.use(
+  response=>response.data,
+  error=>{
+    return promises.reject(error)
+  }
+)
+export default service
