cb277948b4665d37100294e27b3b17b43f917ab8
lpffer <1365166687@qq.com>
2019-06-12 07:33:24 +0800

    更新2

--- manager/src/services/user.js
+++ manager/src/services/user.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import request from '../utils/request';
-
-export function login(params) {
-    console.log(params)
-  return request({
-      method:'POST',
-      data:params,
-      url:'/user/login'
-  });
-}
-=======
 import request from "../utils/request";
 export function login(params) {
     return request({
@@ -18,4 +6,3 @@ export function login(params) {
         data: params
     })
 }
->>>>>>> d02081af8710a8781c163202b8129d7709d06d2a
