068e9ed112e3034169bf52d8a3e003c5221fffdd
unknown <923772138@qq.com>
2019-06-24 16:48:49 +0800

    路由404

--- manager/src/views/Other/403.js
+++ manager/src/views/Other/403.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default ()=>{
   return <div className="other-img">
-    <img src="403.jpg" alt=""/>
+    <img src="http://5b0988e595225.cdn.sohucs.com/q_70,c_zoom,w_640/images/20180703/a611f75f84ce429899f86346d0a2e963.gif" alt=""/>
   </div>
 }
--- manager/src/views/Other/404.js
+++ manager/src/views/Other/404.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { Link } from 'dva/router';
 export default ()=>{
   return <div className="other-img">
-    <Link to="/"><img src="405.gif" alt=""/></Link>  
+    <Link to="/"><img src="http://img.zcool.cn/community/018a7758d24b60a801219c7754463e.gif" alt=""/></Link>  
   </div>
 }
